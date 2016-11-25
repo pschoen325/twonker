cfa019d5f82b34c19f33d0d4b95ba7a386e0a14e
Paul Schoen <pschoen325@gmail.com>
2016-11-25 14:13:58 -0500

    added friends to view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,4 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
-  def new
-  end
-  def show
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @comments = Comment.all
+    @friends = Friendship.all
   end
 end
